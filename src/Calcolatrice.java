11b94b5a5e158385f76e7e3d7baed6a77c9d17c1
Andrea Rossi <radiatore2@gmail.com>
2023-11-02 14:52:57 +0100

    commit

--- src/Calcolatrice.java
+++ src/Calcolatrice.java
@@ -1,4 +1,11 @@
 public class Calcolatrice {
     public static void main(String[] args) {
+        int x = 4;
+        int y = 6;
+        int result = molt(x, y);
+        System.out.println(result);
+    }
+    public static int molt(int x, int y) {
+        return x * y;
     }
 }
