06653bfd3bf6d1d5aecf2ba81e836d1cd393e72a
Alessio Guacci <alessioguacci@gmail.com>
2023-11-03 14:10:17 +0100

    implementazione dei metodi funzioneAddizione e riconoscimentoOperazione

--- src/Calcolatrice.java
+++ src/Calcolatrice.java
@@ -1,27 +1,5 @@
 public class Calcolatrice {
     public static void main(String[] args) {
-        System.out.println(funzioneAddizione(5,7));
-        System.out.println(riconoscimentoOperazione("5-7"));
-    }
-
-    public static int funzioneAddizione(int x, int y){
-        return x+y;
-    }
-
-    public static String riconoscimentoOperazione(String str){
-        if(str.contains("+")){
-            return "SOMMA";
-        } else if (str.contains("-")){
-            return "DIFFERENZA";
-        } else if (str.contains("*")){
-            return "MOLTIPLICAZIONE";
-        } else if (str.contains("/")){
-            return "DIVISIONE";
-        } else if (str.contains("^")){
-            return "POTENZA";
-        } else{
-           return  "ingresso di operazione non riconosciuto";
-        }
 
     }
 }
