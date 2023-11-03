a49bc254b8cea500bcffea42d70a0af5979e3cae
Alessio Guacci <alessioguacci@gmail.com>
2023-11-03 11:30:28 +0100

    implementazione del metodo funzioneAddizione.

--- src/Calcolatrice.java
+++ src/Calcolatrice.java
@@ -1,4 +1,9 @@
 public class Calcolatrice {
     public static void main(String[] args) {
+        System.out.println(funzioneAddizione(5,7));
+    }
+
+    public static int funzioneAddizione(int x, int y){
+        return x+y;
     }
 }
