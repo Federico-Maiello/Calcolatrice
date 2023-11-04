88260a6b6f08aec274035e7e44469fc861f4eaf1
kappa-git <kevin.borgia@outlook.it>
2023-11-04 15:54:20 +0100

    function division

--- src/Calcolatrice.java
+++ src/Calcolatrice.java
@@ -1,21 +1,8 @@
 public class Calcolatrice { 
        public static void main(String[] args) {
-            int dividendo = 18;
-            int divisore = 7;
-            int quoziente = 0;
-            int resto = 0;
-            divisioneConResto(dividendo, divisore, quoziente, resto);
-    }
     
-        public static void divisioneConResto(int dividendo, int divisore, int quoziente, int resto) {
-            quoziente = dividendo / divisore;
-            resto = dividendo % divisore;
-            System.out.println("Quoziente: " + quoziente);
-            System.out.println("Resto: " + resto);
     }
-
-
-    }
+}
         
 
 
