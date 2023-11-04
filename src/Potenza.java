e12f4526d4e542069ac4f0782cd42d08b8cfe1b5
carmentec8 <carmentecchia@gmail.com>
2023-11-04 14:58:59 +0100

    funzione potenza

--- src/Potenza.java
+++ src/Potenza.java
@@ -1,4 +1,5 @@
-public static int calcolaPotenza(int base, int esponente) {
+public class Calcolatrice{
+ public static int calcolaPotenza(int base, int esponente) {
         int risultato = 1;
 
         for (int i = 0; i < esponente; i++) {
@@ -7,3 +8,4 @@ public static int calcolaPotenza(int base, int esponente) {
 
         return risultato;
         }
+}
